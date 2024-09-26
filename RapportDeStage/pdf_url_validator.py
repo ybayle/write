89041123705e4fe877b9e7368e2360a0ed21271a
Yann Bayle <17142748+ybayle@users.noreply.github.com>
2024-09-26 17:22:13 +0200

    add new evaluation

--- RapportDeStage/pdf_url_validator.py
+++ RapportDeStage/pdf_url_validator.py
@@ -2,6 +2,7 @@
 loads pdf file in sys.argv[1], extracts URLs, tries to load each URL
 some credits to stackoverflow.com/questions/27744210
 """
+
 import os
 import sys
 from urllib.error import HTTPError
@@ -13,19 +14,19 @@ import PyPDF2
 def extract_urls(filename):
     """extracts all urls from filename"""
     with open(filename, "rb") as pdf_file:
-        pdf_handler = PyPDF2.PdfFileReader(pdf_file)
-        pages = pdf_handler.getNumPages()
+        pdf_handler = PyPDF2.PdfReader(pdf_file)
+        pages = len(pdf_handler.pages)
 
         key = "/Annots"
         uri = "/URI"
         ank = "/A"
 
         for page in range(pages):
-            page_object = pdf_handler.getPage(page).getObject()
+            page_object = pdf_handler.pages[page].get_object()
             if key in page_object:
                 ann = page_object[key]
                 for value in ann:
-                    object_handler = value.getObject()
+                    object_handler = value.get_object()
                     if uri in object_handler[ank]:
                         yield object_handler[ank][uri]
 
@@ -33,7 +34,22 @@ def extract_urls(filename):
 def check_http_url(link):
     """check url validity"""
     try:
-        req = Request(link, headers={"User-Agent": "Mozilla/5.0"})
+        if "coreus" in link:
+            return
+        # print(link)
+        req = Request(
+            link,
+            headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+                "Accept-Encoding": "none",
+                "Accept-Language": "en-US,en;q=0.8",
+                "Connection": "keep-alive",
+                "refere": "https://google.com",
+                "cookie": """your cookie value ( you can get that from your web page) """,
+            },
+        )
         with urlopen(req, timeout=10) as weburl:
             if weburl.getcode() != 200:
                 print("Broken " + link)
